87dae367dacb33353aba19bf598d26084a374a48
arneetsinghkalra <a.s.kalra1999@gmail.com>
2019-10-07 22:54:39 -0400

    Initial commit of the backend application

---
+++ TutoringService-Backend/src/main/java/ca/mcgill/ecse321/tutoringservice/TutoringServiceApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.tutoringservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutoringServiceApplication.class, args);
+	}
+
+}
