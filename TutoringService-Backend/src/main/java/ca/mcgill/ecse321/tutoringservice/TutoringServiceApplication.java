a3cae0ac34b44ff21cc03cebfc403bc90c054b80
Uyirendiren Chemen <udchemen@gmail.com>
2019-10-11 23:30:50 -0400

    initial commit of backend

--- TutoringService-Backend/src/main/java/ca/mcgill/ecse321/tutoringservice/TutoringServiceApplication.java
+++ TutoringService-Backend/src/main/java/ca/mcgill/ecse321/tutoringservice/TutoringServiceApplication.java
@@ -2,9 +2,12 @@ package ca.mcgill.ecse321.tutoringservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class TutoringServiceApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(TutoringServiceApplication.class, args);
